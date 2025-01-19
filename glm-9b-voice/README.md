727f4ecd4a5ffdfe956a61e0402ea2f0e4e7a377
Anthony Wang <anthony.wang@mygram.ai>
2025-01-19 08:48:39 +0000

    fix: seperate data preprocessing to another file to save vram

---
+++ glm-9b-voice/README.md
@@ -0,0 +1,6 @@
+# Fully finetune GLM-4-voice
+
+```bash
+python build_dataset.py
+deepspeed train.py
+```
--- glm-9b-voice/train.py
+++ glm-9b-voice/train.py
@@ -3,6 +3,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 from transformers import TrainingArguments, Trainer
 import torch
 from datasets import load_dataset
+import pickle
 
 from speech_tokenizer.utils import extract_speech_token
 from src.model import FineTunedGLM4Voice, GLM4VoiceTokenizer, prepare_model_for_lora
@@ -48,24 +49,12 @@ class CustomDataCollator:
             "labels": torch.cat([item["labels"] for item in batch])
         }
 
-whisper_model = WhisperVQEncoder.from_pretrained(tokenizer_path).eval().to(device)
-feature_extractor = WhisperFeatureExtractor.from_pretrained(tokenizer_path)
-
 glm_model = FineTunedGLM4Voice(model_path).model
 glm_tokenizer = GLM4VoiceTokenizer(model_path).tokenizer
 print("Model loaded")
 
-# Prepare `train_texts` consisting of 100 identical audio inputs for debugging
-audio_path = "./input-sample-0.wav"
-audio_tokens = extract_speech_token(
-    whisper_model, feature_extractor, [audio_path]
-)[0]  # 12.5 TPS
-audio_tokens = "".join([f"<|audio_{x}|>" for x in audio_tokens])
-audio_tokens = "<|begin_of_audio|>" + audio_tokens + "<|end_of_audio|>"
-user_input = audio_tokens
-system_prompt = "User will provide you with a speech instruction. Do it step by step. First, think about the instruction and respond in a interleaved manner, with 13 text token followed by 26 audio tokens. "
-prompt = f"<|system|>\n{system_prompt}\n<|user|>\n{user_input}\n<|assistant|>streaming_transcription\n"
-train_texts = [prompt] * 100
+with open("train_texts.pkl", "rb") as f:
+    train_texts = pickle.load(f)
 
 train_dataset = CustomDataset(train_texts, glm_tokenizer)
 print("Dataset loaded")
